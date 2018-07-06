66294ea4053970a5c4c42edb0b431ffdd3fb5799
kchrusciel <krzysztof.chrusciel@outlook.com>
2018-07-06 17:58:27 +0200

    Add properties migrator

---
+++ spring-boot-properties-migrator-example/README.md
@@ -0,0 +1,2 @@
+# Spring-Boot-2-Properties-Migrator-Example
+Spring Boot 2 examples from https://CodeCouple.pl
---
+++ spring-boot-properties-migrator-example/src/main/java/pl/codecouple/propertiesmigrator/PropertiesMigratorApplication.java
@@ -0,0 +1,12 @@
+package pl.codecouple.propertiesmigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesMigratorApplication.class, args);
+	}
+}
