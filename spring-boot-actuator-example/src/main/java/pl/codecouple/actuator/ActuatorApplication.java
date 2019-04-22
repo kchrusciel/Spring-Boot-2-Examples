9eb6dbfd1bdc7ccff19130f7221cd2344378a64f
kchrusciel <krzysztof.chrusciel@outlook.com>
2019-04-22 10:37:03 +0200

    Add actuator

---
+++ spring-boot-actuator-example/src/main/java/pl/codecouple/actuator/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package pl.codecouple.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
+	}
+
+}
